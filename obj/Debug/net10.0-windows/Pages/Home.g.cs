06b930fd05e421ca14635e5fadf0b7a6676e0363
Hinacio Sant <hinaciosant@gmail.com>
2026-05-22 10:34:53 -0300

    Changed gitignore

--- obj/Debug/net10.0-windows/Pages/Home.g.cs
+++ obj/Debug/net10.0-windows/Pages/Home.g.cs
@@ -52,7 +52,7 @@ namespace Aris.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Aris;component/pages/home.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Aris;V1.0.0.0;component/pages/home.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\Home.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
